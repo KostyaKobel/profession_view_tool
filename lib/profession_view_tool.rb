59f9b38de97fea87b3ef3c098c54089e916f8570
KostyaKobel <kostp62020@gmail.com>
2022-01-05 15:30:23 +0200

    Profession tool initial commit

---
+++ lib/profession_view_tool.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+require_relative "profession_view_tool/version"
+require_relative "profession_view_tool/renderer"
+
+module ProfessionViewTool
+  class Error < StandardError; end
+  # Your code goes here...
+end
